3b8f4f6db08e1d2462941b9e39161afbe2e735b0
prodteam <products@polyn.ai>
2026-02-15 22:36:19 +0300

    Update main.c

--- firmware/mcu1_nasp/svcbox743/Core/Src/main.c
+++ firmware/mcu1_nasp/svcbox743/Core/Src/main.c
@@ -2,23 +2,14 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  * @brief          : Main program body (MCU1 NASP-emul MVP0 scaffold)
   ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "dfsdm.h"
+#include "adc.h"
+#include "dma.h"
 #include "i2s.h"
 #include "spi.h"
 #include "tim.h"
@@ -53,92 +44,121 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-void PeriphCommonClock_Config(void);
-/* USER CODE BEGIN PFP */
+static void MPU_Config(void);
+static void MX_NVIC_Init(void);
+static void PeriphCommonClock_Config(void);
 
+/* USER CODE BEGIN PFP */
+static void NComm_App_Init(void);
+static void NComm_App_Tick(void);
+
+/**
+ * @brief MCU1 application entrypoints (implemented later in firmware/mcu1_nasp).
+ * For MVP0 we keep weak stubs so the project links even before you add real code.
+ */
+__weak void ncomm_mcu1_init(void) {}
+__weak void ncomm_mcu1_tick(void) {}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-#ifndef CUBE_MAIN
-void main_init(){
-#else
+
+static void NComm_App_Init(void)
+{
+  // One place to init your new MCU1 logic.
+  // Later here you will start UART protocol (USART3@1M), ADC/I2S acquisition, stream selection, VAD stub.
+  ncomm_mcu1_init();
+}
+
+static void NComm_App_Tick(void)
+{
+  // One place for cooperative "superloop" work.
+  // Later: parse UART cmds, choose stream (MIC_RAW/RX_RAW), send frames, send VAD flags.
+  ncomm_mcu1_tick();
+}
+
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
+#ifdef CUBE_MAIN
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-#endif
-  /* USER CODE END 1 */
-/* Enable the CPU Cache */
+  MPU_Config();
 
-  /* Enable I-Cache---------------------------------------------------------*/
-  SCB_EnableICache();
-
-  /* Enable D-Cache---------------------------------------------------------*/
-  SCB_EnableDCache();
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-/* Configure the peripherals common clocks */
   PeriphCommonClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DFSDM1_Init();
-  MX_TIM2_Init();
-  MX_TIM1_Init();
-  MX_TIM5_Init();
-  MX_TIM4_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
   MX_SPI1_Init();
-  MX_UART4_Init();
-  MX_SPI3_Init();
   MX_I2S2_Init();
+  MX_TIM6_Init();
+  MX_UART4_Init();
   MX_USART3_UART_Init();
-  void ncomm_app_init(void);
-  void ncomm_app_tick(void);
-  /* USER CODE BEGIN 2 */
- 
-  ncomm_uart_init();
-  ncomm_audio_init();
-  ncomm_vad_init();
 
+  MX_NVIC_Init();
+
+  /* USER CODE BEGIN 2 */
+  NComm_App_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-#ifndef CUBE_MAIN
-#else
   while (1)
   {
-  ncomm_uart_poll();
-  ncomm_audio_process();
-  ncomm_vad_process();
+    NComm_App_Tick();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
-#endif
   /* USER CODE END 3 */
 }
+#else
+
+/**
+ * @brief Cube init without owning the superloop.
+ * If you build MCU1 as "library style", call main_init(), then loop and call main_tick().
+ */
+void main_init(void)
+{
+  MPU_Config();
+
+  HAL_Init();
+
+  SystemClock_Config();
+
+  PeriphCommonClock_Config();
+
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  MX_SPI1_Init();
+  MX_I2S2_Init();
+  MX_TIM6_Init();
+  MX_UART4_Init();
+  MX_USART3_UART_Init();
+
+  MX_NVIC_Init();
+
+  NComm_App_Init();
+}
+
+/**
+ * @brief Must be called periodically by external main() if CUBE_MAIN is not used.
+ */
+void main_tick(void)
+{
+  NComm_App_Tick();
+}
+
+#endif /* CUBE_MAIN */
 
 /**
   * @brief System Clock Configuration
@@ -155,11 +175,6 @@ void SystemClock_Config(void)
 
   /** Configure the main internal regulator output voltage
   */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-
-  __HAL_RCC_SYSCFG_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
   while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
@@ -171,12 +186,11 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 467;
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 192;
   RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 15;
+  RCC_OscInitStruct.PLL.PLLQ = 5;
   RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_1;
   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
   RCC_OscInitStruct.PLL.PLLFRACN = 0;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -194,7 +208,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
@@ -204,23 +218,45 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief MPU Configuration.
+  * @retval None
+  */
+static void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+
+  HAL_MPU_Disable();
+
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0x24000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_512KB;
+  MPU_InitStruct.SubRegionDisable = 0x00;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+/**
   * @brief Peripherals Common Clock Configuration
   * @retval None
   */
-void PeriphCommonClock_Config(void)
+static void PeriphCommonClock_Config(void)
 {
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-  /** Initializes the peripherals clock
-  */
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI3|RCC_PERIPHCLK_SPI2
-                              |RCC_PERIPHCLK_SPI1;
-  PeriphClkInitStruct.PLL3.PLL3M = 16;
-  PeriphClkInitStruct.PLL3.PLL3N = 256;
-  PeriphClkInitStruct.PLL3.PLL3P = 5;
-  PeriphClkInitStruct.PLL3.PLL3Q = 2;
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI1;
+  PeriphClkInitStruct.PLL3.PLL3M = 5;
+  PeriphClkInitStruct.PLL3.PLL3N = 160;
+  PeriphClkInitStruct.PLL3.PLL3P = 2;
+  PeriphClkInitStruct.PLL3.PLL3Q = 10;
   PeriphClkInitStruct.PLL3.PLL3R = 2;
-  PeriphClkInitStruct.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_0;
   PeriphClkInitStruct.PLL3.PLL3VCOSEL = RCC_PLL3VCOWIDE;
   PeriphClkInitStruct.PLL3.PLL3FRACN = 0;
   PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL3;
@@ -230,9 +266,22 @@ void PeriphCommonClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
+/**
+  * @brief NVIC Configuration.
+  * @retval None
+  */
+static void MX_NVIC_Init(void)
+{
+  /* USER CODE BEGIN NVIC_Init 0 */
 
-/* USER CODE END 4 */
+  /* USER CODE END NVIC_Init 0 */
+
+  /* DMA interrupts etc are configured in Cube-generated files if enabled */
+
+  /* USER CODE BEGIN NVIC_Init 1 */
+
+  /* USER CODE END NVIC_Init 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -240,28 +289,16 @@ void PeriphCommonClock_Config(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+  (void)file;
+  (void)line;
 }
 #endif /* USE_FULL_ASSERT */
